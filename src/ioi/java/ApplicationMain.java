d234ba76a1ca460ab23f2721bdc1066e1885e306
Tom Kinane <Tom Kinane@LAPTOP-Q0SQGB9H>
2020-01-18 15:24:26 +0000

    v1

---
+++ src/ioi/java/ApplicationMain.java
@@ -0,0 +1,50 @@
+/*
+ To change this license header, choose License Headers in Project Properties.
+ To change this template file, choose Tools | Templates
+ and open the template in the editor.
+ */
+package ioi.java;
+
+/**
+ *
+ * @author Conchraidh
+ */
+public class ApplicationMain
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        {
+            String name="Tom";
+            int age=13;
+            int number1=10;
+            int number2=5;
+            
+            int answer=number1*number2;
+        // One after another    
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(answer);
+        //One 1 line
+            System.out.println("My name is "+ name +" My Age is " + age +" My answer is " + answer);
+            
+            
+        }
+
+//        int counter = 1;
+//        while (counter < 5)
+//        {
+//            System.out.println("Number: " + counter);
+//            counter = counter + 1;
+//        }
+        //System.out.println("1");
+        //System.out.println("2");
+        //System.out.println("3");
+        //System.out.println("4");
+        // TODO code application logic here
+    }
+
+}
